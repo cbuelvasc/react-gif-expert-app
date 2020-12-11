1d0812a175bb336d5c01608fe0daf313bb779d9a
cbuelvasc <cbielvasc@gmail.com>
2020-12-11 17:07:50 -0500

    Update docs and helpers

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,6 +1,6 @@
 
 export const getGifs = async (category) => {
-    const url = `http://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=yil3gBJ6M7bLeQqXimrP0YhYWeAGLMYf`;
+    const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=yil3gBJ6M7bLeQqXimrP0YhYWeAGLMYf`;
     const resp = await fetch(url);
     const { data } = await resp.json();
 
