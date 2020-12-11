9d73b11434c98a830ef25793e1882334b4f85e71
cbuelvasc <cbielvasc@gmail.com>
2020-12-11 16:55:27 -0500

    First commit

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,16 @@
+
+export const getGifs = async (category) => {
+    const url = `http://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=yil3gBJ6M7bLeQqXimrP0YhYWeAGLMYf`;
+    const resp = await fetch(url);
+    const { data } = await resp.json();
+
+    const gifs = data.map(img => {
+        return {
+            id: img.id,
+            title: img.title,
+            url: img.images?.downsized_medium.url
+        }
+    });
+    return gifs;
+}
+
